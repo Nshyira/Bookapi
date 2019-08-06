795101c9ba1522d138e26562c2d02357f732f560
Nshyira <rachealarkorful@gmail.com>
2019-08-06 19:25:27 +0100

    created a books file as database simulator

---
+++ books.js
@@ -0,0 +1,28 @@
+const books = [
+  { id: 1, title: "Harry Potter", author: "Jk Rowling" },
+  { id: 2, title: "Twilight", author: "Stephnie Meyer" },
+  { id: 3, title: "Lorien Legacies", author: "Pittacus Lore" },
+  { id: 4, title: "The Inn", author: "James Patterson" },
+  { id: 5, title: "The turn of the Key", author: "Ruth Ware" },
+  { id: 6, title: "Outfox", author: "Sandra Brown" },
+  { id: 7, title: "Burn after Writing", author: "Sharon Jones" },
+  { id: 8, title: "Unsolved", author: "James Patterson" },
+  { id: 9, title: "The Alchemist", author: "Paulo Coelho" },
+
+  { id: 10, title: "Born a Crime", author: "Trevor Noah" },
+  { id: 11, title: "The Pioneers", author: "David Mucclough" },
+  { id: 12, title: "Lorien Legacies", author: "Pittacus Lore" },
+  { id: 13, title: "Under Currents", author: "Nora Roberts" },
+  { id: 14, title: "Ambush", author: "James Patterson" },
+  { id: 15, title: "Before We Were Yours", author: "Lisa Wingale" }
+
+  /*{ id: 16, title: '', author:''},
+    { id: 17, title: '', author:''},
+    { id: 18, title: '', author:''},
+    { id: 19, title: '', author:''},
+    { id: 20, title: 'Twilight', author:'Stephnie Meyer'},
+    { id: 21, title: 'Lorien Legacies', author:'Pittacus Lore'}
+  */
+];
+
+module.exports.books = books;
